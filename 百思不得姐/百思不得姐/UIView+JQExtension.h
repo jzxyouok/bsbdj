56a219016b701b6da242994fdd6b8ef7f0a8d0b2
王家齐 <wangjiaqi@mzzdxtdemacbook-pro.local>
2016-05-20 09:43:33 +0800

    封装farme

---
+++ 百思不得姐/百思不得姐/UIView+JQExtension.h
@@ -0,0 +1,19 @@
+//
+//  UIView+JQExtension.h
+//  百思不得姐
+//
+//  Created by 王家齐 on 16/5/20.
+//  Copyright © 2016年 王家齐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (JQExtension)
+
+/** 在分类中声明属性，只会生成方法的声明，不会声明方法的实现和带有下划线的成员变量 */
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+
+@end
